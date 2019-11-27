f132f50d37c01cfa20a87f4ff39c7bb522b84089
MatheusCicchetto <mfcicchetto@gmail.com>
2019-11-27 10:23:57 -0200

    add html para home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {AppComponent} from './app.component';
+import {LancamentosComponent} from './lancamentos/lancamentos.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'lancamentos',
+    component: LancamentosComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LancamentosComponent } from './lancamentos/lancamentos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
